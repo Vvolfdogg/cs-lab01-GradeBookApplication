a7a363263428a89406bf45576cef079db3e585d0
Jakub Wilczak <jakub.wilczak@microsoft.wsei.edu.pl>
2022-03-04 11:27:18 +0100

    Po zajeciach

---
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GradeBook.GradeBooks
+{
+    class RankedGradeBook : BaseGradeBook
+    {
+        public RankedGradeBook(string name) : base(name)
+        {
+            base.Type = Enums.GradeBookType.Ranked;
+        }
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+            int studentsPercent = Students.Count;
+
+            foreach(double grade in student.Grades)
+            {
+
+            }
+            return 'A';
+        }
+    }
+}
